e5220802ad79f2cc33b09a35912b2fdcef25eb2d
Person Name <mail@mail.com>
2024-02-26 01:35:45 +1100

    Update Readme

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css'; // Ensure you have this CSS file or remove this line
 import App from './App'; // Ensure you have an App.js component or adjust accordingly
 import reportWebVitals from './reportWebVitals'; // Optional, remove if not using
-import './styles/cipher-weekly.webflow.css';
 import './styles/normalize.css';
 import './styles/webflow.css';
+import './styles/cipher-weekly.webflow.css';
 
 ReactDOM.render(
   <React.StrictMode>
